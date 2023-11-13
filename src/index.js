4bac253ab69a496b63d0337636ad6df4ac38941b
Yevhen-L <ujeen2101@gmail.com>
2023-11-14 00:06:45 +0200

    7

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 import store, { persistor } from 'Redux/store';
-import { App } from './components/App';
+import App from './components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
