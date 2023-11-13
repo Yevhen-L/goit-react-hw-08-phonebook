39f650a9a1f15520d63bc77b4bbca484530fff18
Yevhen-L <ujeen2101@gmail.com>
2023-11-13 22:28:30 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
-import store from './redux/store';
-import { persistor } from 'redux/store';
+
+import store, { persistor } from 'redux/store';
 import { App } from './components/App';
 import './index.css';
 
